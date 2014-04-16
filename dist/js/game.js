1bac2a395c11c2234a8700dc2af21327fd9b8748
Vagios Vlachos <bagios234@gmail.com>
2014-04-16 19:44:51 +0300

    Add moving title on menu screen

--- dist/js/game.js
+++ dist/js/game.js
@@ -78,6 +78,26 @@ Menu.prototype = {
     this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
     // Start scrolling the ground in the negative x direction
     this.ground.autoScroll(-200, 0);
+
+    // Create a group
+    this.titleGroup = this.game.add.group();
+
+    // Create title and bird sprites and add them to titleGroup
+    this.title = this.game.add.sprite(0, 0, 'title');
+    this.bird = this.game.add.sprite(200, 5, 'bird');
+    this.titleGroup.add(this.title);
+    this.titleGroup.add(this.bird);
+
+    // Add animation to bird and start it
+    this.bird.animations.add('flap');
+    this.bird.animations.play('flap', 12, true);
+
+    // Set titleGroup location
+    this.titleGroup.x = 30;
+    this.titleGroup.y = 100;
+
+    // Create oscillating animation tween for the titleGroup
+    this.game.add.tween(this.titleGroup).to({y:115}, 350, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
   },
   update: function() {
     
